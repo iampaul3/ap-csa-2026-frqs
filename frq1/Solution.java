6090802b1069a551796e535a6f309e1882cacdf4
iampaul3 <paull101@nycstudents.net>
2026-05-19 11:41:37 -0400

    Implement Account constructor and getShortenedName method

--- frq1/Solution.java
+++ frq1/Solution.java
@@ -1,1 +1,17 @@
+# Part A
+public Account(String requestedName) {
+  username = requestedName;
+  int i = 0;
+  while (!isAvailable(requestedName)) {
+    i++;
+    username = requestedName += i;
+  }
+}
 
+# Part B
+public String getShortenedName() {
+  if (username.indexOf("-") == -1) {
+      return username;
+  } else {
+    for (int i 
+  }
