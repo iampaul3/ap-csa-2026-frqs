88798f4fc8f2524006f220b2faa76e0ee2c773c7
iampaul3 <paull101@nycstudents.net>
2026-05-20 11:27:55 -0400

    Update Solution.java

--- frq2/Solution.java
+++ frq2/Solution.java
@@ -1,1 +1,18 @@
+public class Bottle{
+    private double capacity;
+    private double filled;
+  
+    public Bottle(double cap){
+        capacity = cap;
+        filled = cap;
+    }
 
+    public double updateAmount(double removed){
+        if((filled-removed) < (capacity/4)){
+            filled = capacity;
+            return capacity;
+        }
+        filled -= removed;
+        return filled;
+    }
+}
