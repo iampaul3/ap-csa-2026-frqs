c80c36dc55d05ddbc43d93815e88e6cf3922e79a
iampaul3 <paull101@nycstudents.net>
2026-05-20 11:43:06 -0400

    Update Solution.java

--- frq3/Solution.java
+++ frq3/Solution.java
@@ -1,1 +1,13 @@
-
+public int moreHistoryThanMathAbsences() {
+  int counter = 0;
+  for (int i = 0; i < historyList.size(); i++) {
+    for (int j = 0; i < mathList.size(); j++) {
+      if (historyList.get(i).getStudentID() == mathList.get(j).getStudentID()) {
+        if (historyList.get(i).getAbsences() > mathList.get(j).getAbsences()) {
+          counter++;
+        }
+      }
+    }
+  }
+  return counter;
+}
