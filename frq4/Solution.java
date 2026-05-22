f837ab27275c553609bdaea1ef69d8c481810fcf
iampaul3 <paull101@nycstudents.net>
2026-05-22 11:27:20 -0400

    Add getPointsForRow method for point calculation
    Implement getPointsForRow method to calculate points based on row color matching.

--- frq4/Solution.java
+++ frq4/Solution.java
@@ -1,1 +1,16 @@
-
+public int getPointsForRow(int targetRow) {
+  int sum = 0;
+  int i = 0;
+  int count = 0;
+  for (int i = 0; i < space.length; i++) {
+    sum += space[targetRow][i];
+  }
+  while (space[targetRow][i].getColor().equals(space[targetRow][i+1].getColor())) {
+    count++;
+  }
+  if (count == space.length) {
+    return 2*sum;
+  } else {
+    return sum;
+  }
+}
